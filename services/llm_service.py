a1008bc9e5dfe06ff742b7e15380010b9c97ce0b
subhakarRPeddakkagar <153081130+subhakarRPeddakkagar@users.noreply.github.com>
2026-04-29 15:46:16 +0530

    Enhance get_llm to include API key
    Add API key retrieval from environment variables.

--- services/llm_service.py
+++ services/llm_service.py
@@ -1,10 +1,9 @@
+import os
 from langchain_groq import ChatGroq
-from dotenv import load_dotenv
-
-load_dotenv()
 
 def get_llm():
     return ChatGroq(
         model="llama-3.1-8b-instant",
-        temperature=0.5
+        temperature=0.5,
+        api_key=os.getenv("GROQ_API_KEY")
     )
